c163e41b9245a9f897b05391d03fe4f86b8d79c4
Mikkel Andreasen <mema@mema.dk>
2022-08-17 16:28:26 +0200

    Working POC

---
+++ ConvertWordToPdf.cs
@@ -0,0 +1,45 @@
+using Azure.Storage.Blobs;
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using GemBox.Document;
+
+namespace memadk.Function
+{
+    public class ConvertWordToPdf
+    {
+        [FunctionName("ConvertWordToPdf")]
+        public async Task Run(
+            [BlobTrigger("documents/{name}")] Stream myBlob,
+            string name, 
+            ILogger log)
+        {
+            string Connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            if(name.EndsWith(".docx"))
+            {
+                log.LogInformation($"Converting {name} to PDF");
+                ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+                var document = DocumentModel.Load(myBlob);
+                var options = SaveOptions.PdfDefault;
+                using (var stream = new MemoryStream())
+                {
+                    document.Save(stream, options);
+                    var pdfName = name.Replace(".docx", ".pdf");
+                    var blobClient = new BlobContainerClient(Connection, "documents");
+                    var blob = blobClient.GetBlobClient(pdfName);
+                    await blob.UploadAsync(stream);
+                }
+
+            }
+            else
+            {
+                log.LogInformation($"{name} is not a word document");
+            }
+            
+        }
+    }
+}
